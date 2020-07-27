58640f3bbd3432139d16f8165ecaf86ae367f855
Fehmi Khenissi <khenissy.fehmy@gmail.com>
2020-07-27 15:37:29 +0200

    first commit

---
+++ src/test/java/fr/integrum/ob/OpenbravoByIntegrumApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.integrum.ob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenbravoByIntegrumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
